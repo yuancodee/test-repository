8752608e62d4cba3c56385b7f804267d79a68c48
yuancode <yuanbingyong@126.com>
2017-05-24 17:59:56 +0800

    add new version to maven center!

---
+++ localrepo/src/main/java/test/com/localrepo/MyTestClass.java
@@ -0,0 +1,16 @@
+package test.com.localrepo;
+
+import android.util.Log;
+
+/**
+ * Function:
+ *
+ * @author 袁秉勇
+ * @since 2017年05月24日
+ */
+
+public class MyTestClass {
+    public static void TestMethod() {
+        Log.e("yby_log", "it is dangerous!!");
+    }
+}
